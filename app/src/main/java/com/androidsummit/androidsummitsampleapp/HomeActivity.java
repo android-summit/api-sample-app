b726eb61314c6a48a9760e57ba14dd9c9eb138ec
Michael Ferraco <michael.ferraco@capitalone.com>
2016-08-15 15:01:16 -0400

    Initial project setup, updated gitignore

---
+++ app/src/main/java/com/androidsummit/androidsummitsampleapp/HomeActivity.java
@@ -0,0 +1,10 @@
+package com.androidsummit.androidsummitsampleapp;
+
+/**
+ * The initial activity that opens when starting the app.  Select an API from this activity to be taken to that APIs implementation page.
+ */
+public class HomeActivity {
+
+    
+
+}
