600b93a0b1dd273295fdc430c02b2659314b4ce8
Michael Ferraco <michael.ferraco@capitalone.com>
2016-08-16 15:59:04 -0400

    Added Nessie dependency to the project and api key in gradle.properties

--- app/src/main/java/com/androidsummit/androidsummitsampleapp/NessieActivity.java
+++ app/src/main/java/com/androidsummit/androidsummitsampleapp/NessieActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.reimaginebanking.api.nessieandroidsdk.requestclients.NessieClient;
+
 /**
  * This activity demonstrates usage of the Nessie API - Capital One's hackathon API.
  *
@@ -12,9 +14,14 @@ import android.support.v7.app.AppCompatActivity;
  */
 public class NessieActivity extends AppCompatActivity {
 
+    NessieClient mClient;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nessie_activity);
+
+        String key = BuildConfig.NESSIE_API_KEY;
+        mClient = NessieClient.getInstance(key);
     }
 }
